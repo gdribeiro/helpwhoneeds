a78a49b54b7c60addab1e4de19d3b4b252caa6d7
JonathanSilvaMartinsCAN <jonathansilvamartinscanada@gmail.com>
2017-05-29 16:39:25 -0300

    Created projects

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170528202800) do
+ActiveRecord::Schema.define(version: 20170528230557) do
 
   create_table "donations", force: :cascade do |t|
     t.string "description"
@@ -20,6 +20,15 @@ ActiveRecord::Schema.define(version: 20170528202800) do
     t.datetime "updated_at", null: false
     t.integer "user_id"
     t.index ["user_id"], name: "index_donations_on_user_id"
+  end
+
+  create_table "projects", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.decimal "goal_amount"
+    t.decimal "current_amount"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
