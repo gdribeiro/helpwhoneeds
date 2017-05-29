fceb3a97f33dfa15b2c4f239c8ead2ece698ab80
JonathanSilvaMartinsCAN <jonathansilvamartinscanada@gmail.com>
2017-05-29 17:43:12 -0300

    Changed the fields on donations index and added a few donations in seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+donations = Donation.create([{description: "Doação ao Médicos Sem Fronteiras", value: 1500.00}, 
+	{description: "Donation to Doctors Without Borders", value: 1200.00, user_id: 1}, 
+	{description: "Donation to Doctors Without Borders", value: 800.00, user_id: 1}, 
+	{description: "Donation to Doctors Without Borders", value: 150.00, user_id: 1}, 
+	{description: "Donation to Doctors Without Borders", value: 300.00, user_id: 1},
+	{description: "Donation to Bill And Melinda Gates Foundation", value: 1500.00, user_id: 1},
+	{description: "Donation to Bill And Melinda Gates Foundation", value: 3000.00, user_id: 1},
+	{description: "Donation to Bill And Melinda Gates Foundation", value: 500.00, user_id: 1},
+	{description: "Donation to Bill And Melinda Gates Foundation", value: 800.00, user_id: 1},
+	{description: "Donation to Feeding America", value: 1000.00, user_id: 1},
+	{description: "Donation to Feeding America", value: 1300.00, user_id: 1},
+	{description: "Donation to Feeding America", value: 100.00, user_id: 1},
+	{description: "Donation to Feeding America", value: 300.00, user_id: 1},
+	{description: "Donation to Feeding America", value: 200.00, user_id: 1},
+	{description: "Doação ao Médicos Sem Fronteiras", value: 400.00, user_id: 1},
+	{description: "Doação ao Médicos Sem Fronteiras", value: 1800.00, user_id: 1},
+	{description: "Doação ao Médicos Sem Fronteiras", value: 100.00, user_id: 1},
+	{description: "Doação ao Médicos Sem Fronteiras", value: 180.00, user_id: 1}])
